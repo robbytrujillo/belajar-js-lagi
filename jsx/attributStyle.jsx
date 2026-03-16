daad48c77e49f48c56d490efd29912e65e16d916
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-16 08:21:17 +0700

    introduction JS basic | atribut JSX

---
+++ jsx/attributStyle.jsx
@@ -0,0 +1,10 @@
+// 1. Memberi gaya pada elemen JSX
+// Mirip seperti CSS pada HTML, CSS/style bisa diterapkan di JSX
+// aturan penulisan style di JSX:
+// a. objek js->nilai dari attribut style harus berupa objek JS,
+// b. Camel Case: nama properti CSS yang menggunakan tanda hubung (hypen) di HTML seperti background-color menjadi backgroundColor
+// c. Double Curly Braces: Atribut style harus ditulis di dalam double curly bracess {{ ... }}
+
+const element = (
+  <h1 style={{ color: "red", backgroundColor: "yellow" }}>Halo, dunia!</h1>
+);
